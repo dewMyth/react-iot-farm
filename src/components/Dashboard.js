cf9066df3ad7378c4d307300baf88ad8b3506f33
Dewmith Akalanka <akalankadewmith@gmail.com>
2021-08-11 10:41:38 +0530

    Added Keep Records and loading screens

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,101 +1,197 @@
-import React, { useEffect, useState } from 'react'
-import { useAuth } from '../contexts/AuthContext'
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 
-import {db, fs} from "../firebase.config"
+import { db, fs } from "../firebase.config";
 
 //Bootstrap Imports
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Spinner } from "react-bootstrap";
 
-import DashboardNavbar from './DashboardNavbar'
-import LiveDataCard from './LiveDataCard';
+import DashboardNavbar from "./DashboardNavbar";
+import LiveDataCard from "./LiveDataCard";
+import VoltageLineChart from "./VoltageLineChart";
 
-import "../css/style.css"
+import "../css/style.css";
 
 const Dashboard = () => {
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [deviceId, setDeviceId] = useState("");
+  const [liveData, setLiveData] = useState({});
+  const [records, setRecords] = useState([]);
 
-    const [error, setError] = useState("")
-    const [deviceId, setDeviceId] = useState("")
-    const [liveData, setLiveData] = useState({})
-    const {logout, currentUser} = useAuth();
-    const history = useHistory()
+  const { logout, currentUser } = useAuth();
+  const history = useHistory();
 
-    //Humidity
-    const humidity_text = "Humidity"
-    const humidity_color = "border-left-primary"
-    const humidity_icon = "fas fa-tint"
+  //Humidity
+  const humidity_text = "Humidity";
+  const humidity_color = "border-left-primary";
+  const humidity_icon = "fas fa-tint";
 
-    //Solar Status
-    const solar_status_text = "Solar Status"
-    const solar_status_color = "border-left-warning"
-    const solar_status_icon = "fas fa-solar-panel"
-    
-    //Moisture Status
-    const moisture_text = "Moisture"
-    const moisture_color = "border-left-success"
-    const moisture_icon = "fas fa-water"    
+  //Solar Status
+  const solar_status_text = "Solar Status";
+  const solar_status_color = "border-left-warning";
+  const solar_status_icon = "fas fa-solar-panel";
 
-    //Voltage Status
-    const voltage_text = "Voltage"
-    const voltage_color = "border-left-danger"
-    const voltage_icon = "fas fa-bolt"       
+  //Moisture Status
+  const moisture_text = "Moisture";
+  const moisture_color = "border-left-success";
+  const moisture_icon = "fas fa-water";
 
-    useEffect(() => {
-        fs.collection('users').doc(currentUser.uid).get()
-        .then(snapshot => {
-            setDeviceId(snapshot.data().deviceId)
-            if(deviceId === null){
-                //handle here
+  //Voltage Status
+  const voltage_text = "Voltage";
+  const voltage_color = "border-left-danger";
+  const voltage_icon = "fas fa-bolt";
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await fs
+          .collection("users")
+          .doc(currentUser.uid)
+          .get()
+          .then((snapshot) => {
+            setDeviceId(snapshot.data().deviceId);
+            if (deviceId === null) {
+              //handle here
+            } else {
+              db.ref("FirebaseIOT/Live_data/" + snapshot.data().deviceId).on(
+                "value",
+                function (snapshot) {
+                  setLiveData(snapshot.val());
+                }
+              );
             }
-            else {
-                db.ref('FirebaseIOT/Live_data/'+ snapshot.data().deviceId).on('value', (snapshot) => {
-                    setLiveData(snapshot.val())
-                })
-            }
-        }
-        )
-     })
+          });
+        setLoading(true);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
 
-    async function handleLogout(){ 
-        setError('');
-        try{
-            await logout();
-            history.push('/login')
-        }catch{
-            setError('cannot logout')
-        }
+  useEffect(() => {
+    (async () => {
+      try {
+        await db
+          .ref("FirebaseIOT/Keep_record/1001")
+          .once("value", function (snapshot) {
+            let tempRecordArray = [];
+            snapshot.forEach(function (childsnapshot) {
+              if (childsnapshot.val().voltage) {
+                let tempObj = {
+                  id: childsnapshot.key,
+                  humidity: childsnapshot.val().humidity,
+                  light: childsnapshot.val().light,
+                  moisture: childsnapshot.val().moisture,
+                  motor_status: childsnapshot.val().motor_status,
+                  solar_status: childsnapshot.val().solar_status,
+                  temperature: childsnapshot.val().temperature,
+                  voltage: childsnapshot.val().voltage,
+                };
+                tempRecordArray.push(tempObj);
+              }
+            });
+            setRecords(tempRecordArray);
+          });
+        console.log("Records Updated");
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
+
+  async function handleLogout() {
+    setError("");
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("cannot logout");
     }
+  }
 
-    return (
-        <React.Fragment>
-            <DashboardNavbar handleLogout={handleLogout} error={error} currentUser={currentUser}/>
-            <Container>
-                <h1>
-                    My Device = {deviceId}
-                </h1>
-              <br/>
-              <Container>
-                <Row>
-                  <Col sm>                    
-                    <LiveDataCard color={humidity_color} text={humidity_text} icon={humidity_icon} value={liveData.humidity}/>
-                  </Col>
-                  <div className="mobile-space"><br/></div>
-                  <Col sm>                                        
-                    <LiveDataCard color={moisture_color} text={moisture_text} icon={moisture_icon} value={liveData.moisture}/>
-                  </Col>
-                  <div className="mobile-space"><br/></div>
-                  <Col sm>
-                    <LiveDataCard color={voltage_color} text={voltage_text} icon={voltage_icon} value={liveData.voltage}/>
-                  </Col>
-                  <div className="mobile-space"><br/></div>
-                  <Col sm>
-                    <LiveDataCard color={solar_status_color} text={solar_status_text} icon={solar_status_icon} value={liveData.solar_status}/>
-                  </Col>          
-                </Row>
-              </Container>              
-            </Container>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <DashboardNavbar
+        handleLogout={handleLogout}
+        error={error}
+        currentUser={currentUser}
+      />
+      <Container>
+        <Row className="mt-4">
+          <Col>
+            <h1>Current Stats</h1>
+          </Col>
+          <Col className="text-right">
+            <h1>
+              My Device : {loading ? deviceId : <Spinner animation="grow" />}
+            </h1>
+          </Col>
+        </Row>
 
-export default Dashboard
+        <hr />
+        <br />
+        <Container>
+          <Row>
+            <Col sm>
+              <LiveDataCard
+                color={humidity_color}
+                text={humidity_text}
+                icon={humidity_icon}
+                value={liveData.humidity}
+                loading={loading}
+              />
+            </Col>
+            <div className="mobile-space">
+              <br />
+            </div>
+            <Col sm>
+              <LiveDataCard
+                color={moisture_color}
+                text={moisture_text}
+                icon={moisture_icon}
+                value={liveData.moisture}
+                loading={loading}
+              />
+            </Col>
+            <div className="mobile-space">
+              <br />
+            </div>
+            <Col sm>
+              <LiveDataCard
+                color={voltage_color}
+                text={voltage_text}
+                icon={voltage_icon}
+                value={liveData.voltage}
+                loading={loading}
+              />
+            </Col>
+            <div className="mobile-space">
+              <br />
+            </div>
+            <Col sm>
+              <LiveDataCard
+                color={solar_status_color}
+                text={solar_status_text}
+                icon={solar_status_icon}
+                value={liveData.solar_status}
+                loading={loading}
+              />
+            </Col>
+          </Row>
+        </Container>
+
+        <Container className="mt-4">
+          <Row>
+            <Col md={6}>
+              <VoltageLineChart records={records} />
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/VoltageLineChart.js
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const VoltageLineChart = (props) => {
+  return (
+    <React.Fragment>
+      <div class="card shadow mb-4">
+        {/* <!-- Card Header - Dropdown --> */}
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+          {/* <div class="dropdown no-arrow">
+              <a
+                class="dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+              </a>
+              <div
+                class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                aria-labelledby="dropdownMenuLink"
+              >
+                <div class="dropdown-header">Dropdown Header:</div>
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </div> */}
+        </div>
+        {/* <!-- Card Body --> */}
+        <div class="card-body">
+          <div class="chart-area">
+            <LineChart
+              width={500}
+              height={300}
+              data={props.records}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="id" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="voltage"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default VoltageLineChart;
