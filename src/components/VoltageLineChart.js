ed1f4abaf272ccbcf4fae2e2a2c3ac16532c4e23
Dewmith Akalanka <akalankadewmith@gmail.com>
2021-08-11 11:13:28 +0530

    fixed mobile view chart

--- src/components/VoltageLineChart.js
+++ src/components/VoltageLineChart.js
@@ -16,7 +16,7 @@ const VoltageLineChart = (props) => {
       <div class="card shadow mb-4">
         {/* <!-- Card Header - Dropdown --> */}
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-          <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+          <h6 class="m-0 font-weight-bold text-primary">Voltage Overview</h6>
           {/* <div class="dropdown no-arrow">
               <a
                 class="dropdown-toggle"
@@ -49,30 +49,32 @@ const VoltageLineChart = (props) => {
         </div>
         {/* <!-- Card Body --> */}
         <div class="card-body">
-          <div class="chart-area">
-            <LineChart
-              width={500}
-              height={300}
-              data={props.records}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="id" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Line
-                type="monotone"
-                dataKey="voltage"
-                stroke="#8884d8"
-                activeDot={{ r: 8 }}
-              />
-            </LineChart>
+          <div class="chart-area" style={{ width: "99%", height: "99%" }}>
+            <ResponsiveContainer>
+              <LineChart
+                width={500}
+                height={300}
+                data={props.records}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="id" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="voltage"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
           </div>
         </div>
       </div>
